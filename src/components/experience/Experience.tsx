3b113964e8f4b7cac79a869817b3afd0c410057b
Vika <Vikasatta1998@gmail.com>
2022-05-06 11:39:10 +0300

    first

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './experience.css'
+import {GiSkills} from "react-icons/gi";
+import {MdMiscellaneousServices} from "react-icons/md";
+
 const Experience = () => {
     return (
         <section id={'experience'}>
-            Experience
+            <h5>What Skills I Have</h5>
+            <h2>My Experience</h2>
+            <div className="container experience__container">
+                <div className="experience__hard">  {/*coding*/}
+                    <h3>Hard Skills</h3>
+                    <div className="experience__content">
+                        <article className='experience__details'>
+                            <MdMiscellaneousServices/>
+                        </article>
+                    </div>
+                </div>
+                <div className="experience__soft">{/*характер*/}
+                    <h3>Soft Skills</h3>
+                    <GiSkills/>
+                </div>
+            </div>
         </section>
     );
 };
